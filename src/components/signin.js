7ccb426502fd34209f4839e62e4db1613b57f6d7
jmshanahan <comeraghsolutions@gmail.com>
2018-08-31 14:50:05 +0100

    Added pretty quick

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,10 +1,7 @@
+import React, { Fragment } from 'react';
 
-import React, {Fragment}from "react";
-
-
-
-export default  () => (
-    <Fragment>
-        <h1>Sign in</h1>
-    </Fragment>
-)
+export default () => (
+  <Fragment>
+    <h1>Sign in</h1>
+  </Fragment>
+);
